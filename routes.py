25b5913ea4a35fd99457d2148974bc6b7785689f
Sindy0711 <136922361+Sindy0711@users.noreply.github.com>
2024-03-05 22:59:29 +0700

    05/03

--- routes.py
+++ routes.py
@@ -18,7 +18,8 @@ def login_required(f):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+        session.clear()
+        return render_template('index.html')
 
 
 
@@ -60,8 +61,12 @@ def login():
         
         email = request.form.get("email")
         password = request.form.get("password")
-
+        
         user = User.query.filter_by(email=email).first()
+        if user is None:
+            flash('User does not exist. Please check your email.', 'danger')
+            return redirect(url_for('index'))
+
         if user and check_password_hash(user.password, password):
             session['user_id'] = user.id
             session['first_name'] = user.first_name
@@ -83,43 +88,43 @@ def logout():
 
 @app.route('/home', methods=["GET", "POST"])
 def home():
-    if request.method != "POST":
-        return render_template('home.html')
-    name = request.form.get('name')
-    code = request.form.get('code')
-    join = request.form.get('join', False)
-    create = request.form.get('create', False)
+    if request.method == "POST":
+        name = request.form.get('name')
+        create = request.form.get('create', False)
+        code = request.form.get('code')
+        join = request.form.get('join', False)
 
-    if not name:
-        return render_template('home.html', error="Name is required", code=code)
+        if not name:
+            return render_template('home.html', error="Name is required", code=code)
 
-    if create != False:
-        new_code = generate_room_code(6, [room.code for room in Room.query.all()])
-        room = Room(code=new_code)
-        db.session.add(room)
-        db.session.commit()
-        flash('Room created successfully.', 'success')
-        return redirect(url_for('room', room_code=new_code))
-
-    if join != False:
-        if not code:
-            return render_template('home.html', error="Please enter a room code to enter a chat room", name=name)
-
-        if "user_id" not in session:
-            flash('You need to login to join a room.', 'error')
-            return redirect(url_for('login'))
-        if room := Room.query.filter_by(code=code).first():
-            new_join = Join(user_id=session.get("user_id"), room_id=room.id)
-            db.session.add(new_join)
+        if create != False:
+            room_code = generate_room_code(6, [room.code for room in Room.query.all()])
+            room = Room(code=room_code)
+            db.session.add(room)
             db.session.commit()
-            flash('Joined room successfully.', 'success')
+            flash('Room created successfully.', 'success')
+            session['room'] = room_code
+            session['name'] = name
+            return redirect(url_for('room'))
 
-        else:
-            flash('Room does not exist.', 'error')
+        if join != False:
+            if not code:
+                return render_template('home.html', error="Please enter a room code to enter a chat room", name=name)
 
-    # Redirect đến route chat room với mã phòng đã nhập
-    return redirect(url_for('room', room_code=code))
-    
+            room = Room.query.filter_by(code=code).first()
+            if room:
+                new_join = Join(user_id=session.get("user_id"), room_id=room.id)
+                db.session.add(new_join)
+                db.session.commit()
+                flash('Joined room successfully.', 'success')
+                session['room'] = code
+                session['name'] = first_name
+                return redirect(url_for('room'))
+            else:
+                flash('Room does not exist.', 'error')
+
+    return render_template('home.html')
+
 
 
 
@@ -128,13 +133,13 @@ def room():
     room_code = session.get('room')
     name = session.get('name')
 
-    if name is None or room is None:
+    if name is None or room_code is None:
         return redirect(url_for('home'))
 
     # Check if room exists in the database
     room = Room.query.filter_by(code=room_code).first()
 
-    if room is None:
+    if room_code is None:
         return redirect(url_for('home'))
 
     messages = Message.query.filter_by(room_id=room.id).all()
@@ -144,19 +149,34 @@ def room():
 @socketio.on('connect')
 def handle_connect():
     room_code = session.get('room')
-    name = session.get('name')
+    name = session.get('first_name')
 
     if name is None or room_code is None:
         return
-    if room_code not in rooms:
-        leave_room(room_code)
+    
+    room = Room.query.filter_by(code=room_code).first()
+    
+    if room is None:
+        flash('Room does not exist.', 'error')
+        return
+
+    user = User.query.filter_by(first_name=name).first()
+    if user:
+        join_record = Join.query.filter_by(user_id=user.id, room_id=room.id).first()
+        if not join_record:
+            new_join = Join(user_id=user.id, room_id=room.id)
+            db.session.add(new_join)
+            db.session.commit()
 
     join_room(room_code)
     send({
         "sender": "",
         "message": f"{name} has entered the chat"
     }, to=room_code)
-    rooms[room_code]["members"] += 1
+    
+    num_members = Join.query.filter_by(room_id=room.id).count()
+    room.members = num_members  # Cập nhật số lượng thành viên vào phòng
+    db.session.commit()
 
 
 @socketio.on('message')
@@ -171,14 +191,14 @@ def handle_message(payload):
     message_content = payload.get("message")
     if message_content:
         room = Room.query.filter_by(code=room_code).first()
-        if room :
+        if room:
             message = Message(content=message_content, room_id=room.id)
             db.session.add(message)
             db.session.commit()
 
             message_data = {
                 "sender": name,
-                "message": f"{name} has entered the chat"
+                "message": message_content
             }
             send(message_data, to=room_code)
         else:
@@ -189,27 +209,38 @@ def handle_message(payload):
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    room = session.get("room")
-    name = session.get("name")
+    room_code = session.get('room')
+    name = session.get('name')
 
-    if room in rooms:
-        leave_room(room)
-        rooms[room]["members"] -= 1
-
-        # Kiểm tra xem có ai còn trong phòng không
-        if rooms[room]["members"] <= 0:
-            del rooms[room]
-
-            # Xóa phòng ra khỏi cơ sở dữ liệu khi không còn ai trong phòng
+    room = Room.query.filter_by(code=room_code).first()
+    
+    if room:
+        user = User.query.filter_by(first_name=name).first()
+        if user:
+            join_record = Join.query.filter_by(user_id=user.id, room_id=room.id).first()
+            if join_record:
+                try:
+                    db.session.delete(join_record)
+                    db.session.commit()
+                    print("User left room:", name)
+                except Exception as e:
+                    print("Error deleting user from room:", e)
+        
+        # Kiểm tra xem còn ai trong phòng không
+        num_members = Join.query.filter_by(room_id=room.id).count()
+        if num_members <= 0:
             try:
-                db.execute(text("DELETE FROM rooms WHERE room_code = :room_code"),
-                        {"room_code": room})
-                db.commit()
-                print("Room deleted from database:", room)
+                Message.query.filter_by(room_id=room.id).delete()
+                db.session.delete(room)
+                db.session.commit()
+                print("Room deleted from database:", room_code)
             except Exception as e:
                 print("Error deleting room from database:", e)
         else:
-            print("Room still has members:", room)
-
+            print("Room still has members:", room_code)
     else:
-        print("Room not found in rooms:", room)
+        flash('Room does not exist.', 'error')
+    session.clear()
+    flash('You were successfully logged out')
+        
+    return redirect(url_for('home'))
